6111ea477022522019246b2c3a47237efa507ada
savanna chiang <savannaac@gmail.com>
2022-07-01 23:45:37 -0500

    creates users controller and actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
     protected
 
-    # adds name input to sign up
+    # adds name field to sign up
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
