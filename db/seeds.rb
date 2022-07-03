b7e5a869c07bc6b89f0b79e222c95c282f221498
savanna chiang <savannaac@gmail.com>
2022-07-03 18:44:35 -0500

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# adnat seeds
+
+# organizations = Organization.create([{ name: "Tea Time", hourly_rate: 15 }, { name: "Rock and Bowl", hourly_rate: 17 }])
+
+# User.create([
+#     { 
+#         organization_id: 1, 
+#         name: "Sav", 
+#         email: "savannaac@gmail.com", 
+#         password: "12345"
+#     },
+#     {
+#         organization_id: 1, 
+#         name: "CHRIS", 
+#         email: "chris@gmail.com", 
+#         password: "303"
+#     }
+# ])
+
+# Shift.create([
+#     {
+#         organization_id: 1, 
+#         user_id: 1, 
+#         start: DateTime.new(2022, 7, 03, 10), 
+#         finish: DateTime.new(2022, 7, 03, 16), 
+#         break_length: 30  
+#     },
+#     {
+#         organization_id: 1, 
+#         user_id: 2, 
+#         start: DateTime.new(2022, 7, 04, 11), 
+#         finish: DateTime.new(2022, 7, 04, 17), 
+#         break_length: 60  
+#     }
+# ])
